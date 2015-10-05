0a9c6708f6cb35ea5aa4b0050ba398b2b00071be
Chris Vogt <mail@chrisvogt.me>
2015-10-05 00:55:12 -0700

    (front-end) Updates the browser template adding a header bar showing the author username.

--- src/gistbrowser.js
+++ src/gistbrowser.js
@@ -4,71 +4,92 @@
  * Copyright (c) 2015 Chris Vogt
  * Licensed under the MIT license.
  */
-(function ($) {
-  $.fn.GistBrowser = function (username) {
-    return this.each(function (i) {
-      var username = extractData('username', this),
-      	  query    = buildQuery(username);
+(function($) {
+    $.fn.GistBrowser = function(_username) {
+        return this.each(function(i) {
+            var username = extractData('username', this),
+                query = buildQuery(username);
 
-      /**
-       * "Hold on to your butts," 
-       */
-      makeCall(query, this);
+            /**
+             * "Hold on to your butts," 
+             */
+            makeCall(query, this);
+            reloadInterface();
 
-      /**
-       * Extract data attributes in the form of `data-gist-*`.
-       *
-       * @param {string}
-       * @param {Object}
-       */
-      function extractData(field, e) {
-      	return $(e).attr('data-gist-' + field)
-      }
+            function determineUser() {
+                // do something magical
+            }
 
-      /**
-       * Builds the query string.
-       *
-       * @param {string}
-       */
-	  function buildQuery(u) {
-	  	var tpl = "https://api.github.com/users/" + u + "/gists";
-	  	return tpl;
-	  }
+        	function reloadInterface() {
+        		console.log('Todo: Find the header element for reloadInterface() ...');
+        		$("#browser p.title").val("Browsing files: <em>" + username + "</em>");
+        	}
 
-	  /**
-	   * Makes the AJAX call.
-	   *
-	   * @param {string}
-	   * @param {Object}
-	   */
-	  function makeCall(q, e) {
-		  return $.ajax({
-		      // url: q,
-		      url: "_mock.json",
-		      success: function(data) {
-		      	var html = $(e).empty(),
-		      		gists = data,
-		      		files = [];
+            /**
+             * Extract data attributes in the form of `data-gist-*`.
+             *
+             * @param {string}
+             * @param {Object}
+             */
+            function extractData(field, e) {
+                return $(e).attr('data-gist-' + field)
+            }
 
-		      	for (var i = 0; i < gists.length; i++) {
-		      		var gist = gists[i];
+            /**
+             * Builds the query string.
+             *
+             * @param {string}
+             */
+            function buildQuery(u) {
+                var tpl = "https://api.github.com/users/" + u + "/gists";
+                return tpl;
+            }
 
-					$.each(gist['files'], function(k, v) {
-					    files.push(v);
-					});
-		      	}
+            /**
+             * Makes the AJAX call.
+             *
+             * @param {string}
+             * @param {Object}
+             */
+            function makeCall(q, e) {
+                return $.ajax({
+                    // url: q,
+                    url: "_mock.json",
+                    success: function(data) {
+                        var html = $(e).empty(),
+                            gists = data,
+                            files = [];
 
-		      	$(e).append($("<ul>", {id: "files"}));
+                        for (var i = 0; i < gists.length; i++) {
+                            var gist = gists[i];
 
-		      	for (var i = 0; i < files.length; i++) {
-		      		var file = files[i];
-		      		console.log(file);
-		      		$("#files").append($("<li><span class=\"octicon octicon-gist\"></span> " + file['filename'] + "</li>", file));
-		      	}
+                            $.each(gist['files'], function(k, v) {
+                                files.push(v);
+                            });
+                        }
 
-		      }
-		  });
-	  }
+                        $(e).append($("<ul>", {
+                            id: "files"
+                        }));
+
+                        $("#files").append( "<header><p class=\"title\">Gists by <strong>" + username + "</strong>, chronological</p></header>" );
+
+                        for (var i = 0; i < files.length; i++) {
+                            var file = files[i];
+                            $("#files").append($("<li><span class=\"octicon octicon-gist\"></span> " + file['filename'] + "</li>", file));
+                        }
+
+                    }
+                });
+            }
+        });
+    };
+    /**
+     * Click handler
+     */
+    $("#control a").click(function(event) {
+        event.preventDefault();
+        console.log("Attempting to trigger GistBrowser reinitialization...");
+        $("#browser").GistBrowser($("#control input").val());
     });
-  };
 }(jQuery));
